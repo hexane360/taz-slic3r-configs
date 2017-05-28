18e96dbde8a4954489b0b8c08e3ad8934b372c40
Colin Gilgenbach <colin@gilgenbach.net>
2017-05-28 13:52:48 -0600

    Makes batch script path-agnostic

--- dual_to_single_fan.bat
+++ dual_to_single_fan.bat
@@ -3,18 +3,22 @@ setlocal EnableDelayedExpansion
 
 FOR %%f IN (%*) DO (
 	IF EXIST %%~ff% (
-		echo Received file "%%~nxf"
-		type "%%~ff" | call repl.bat "^(M106 S(\d*).*)$" "$1\nM106 P1 S$2 ;single extruder fan" X M >"%%~ff.1" && (
+		echo Received file "%%~ff"
+		echo Modifying M106. . .
+		type "%%~ff" | call "%~dp0repl.bat" "^(M106 S(\d*).*)$" "$1\nM106 P1 S$2 ;single extruder fan" X M >"%%~ff.1" && (
 			move /y "%%~ff.1" "%%~ff"
 		) || (
 			del "%%~ff.1"
 			echo Failed to run M106 script
 		)
-		type "%%~ff" | call repl.bat "^(M107(\D.*)?)$" "$1\nM107 P1 ;single extruder fan" X M >"%%~ff.1" && (
+		echo Modifying M107. . .
+		type "%%~ff" | call "%~dp0repl.bat" "^(M107(\D.*)?)$" "$1\nM107 P1 ;single extruder fan" X M >"%%~ff.1" && (
 			move /y "%%~ff.1" "%%~ff"
 		) || (
 			del "%%~ff.1"
 			echo Failed to run M107 script
 		)
+	) ELSE (
+		echo Unable to find file "%%~ff", skipping
 	)
 )
