66696c976894782c738d2209f87af59dd4fbd429
Colin Gilgenbach <colin@gilgenbach.net>
2017-05-28 13:03:07 -0600

    Fixed batch script to insert alternative fan commands

--- dual_to_single_fan.bat
+++ dual_to_single_fan.bat
@@ -4,7 +4,17 @@ setlocal EnableDelayedExpansion
 FOR %%f IN (%*) DO (
 	IF EXIST %%~ff% (
 		echo Received file "%%~nxf"
-		type "%%~ff" | call repl.bat "^(M106 S(\d*).*)$" "${1}\nM106 P1 S${2} ;single extruder fan" M X >"%%~ff.1"
-		move /y "%%~ff.1" "%%~ff"
+		type "%%~ff" | call repl.bat "^(M106 S(\d*).*)$" "$1\nM106 P1 S$2 ;single extruder fan" X M >"%%~ff.1" && (
+			move /y "%%~ff.1" "%%~ff"
+		) || (
+			del "%%~ff.1"
+			echo Failed to run M106 script
+		)
+		type "%%~ff" | call repl.bat "^(M107(\D.*)?)$" "$1\nM107 P1 ;single extruder fan" X M >"%%~ff.1" && (
+			move /y "%%~ff.1" "%%~ff"
+		) || (
+			del "%%~ff.1"
+			echo Failed to run M107 script
+		)
 	)
 )
