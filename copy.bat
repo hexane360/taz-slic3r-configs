64916cc4878f5ac6002fffaa7a20625173cf64c8
Colin Gilgenbach <colin@gilgenbach.net>
2017-06-01 11:59:33 -0600

    Improved batch scripts, added filament material information, and standardized to FW retraction and slicer acceleration

--- copy.bat
+++ copy.bat
@@ -1,18 +1,21 @@
 @echo off
 setlocal EnableDelayedExpansion
-
 :finddrive
 FOR %%d in (G F E) DO (
 	IF EXIST "%%d:"\* (
-		SET drive=%%d:
-		echo Using drive %%d:
-		goto copyfiles
+		IF NOT "%%d:"=="%~d0" (
+			SET drive=%%d:
+			goto copyfiles
+		)
 	)
 )
 echo No drive found.
 goto end
 
 :copyfiles
+
+for /f "tokens=2" %%a in ('wmic volume get Label^, DriveLetter ^| find "%drive%"') do set label=%%a
+echo Using drive %drive% (%label%)
 FOR %%f IN (%*) DO (
 	IF EXIST %%~ff% (
 		echo Received file "%%~nxf"
--- dual_to_single_fan.pl
+++ dual_to_single_fan.pl
@@ -3,8 +3,8 @@
 use strict;
 use warnings;
 
-our $^I = '.bak';
 while ( <> ) {
-   s/^(M106 S(\d*).*)$/${1}\nM106 P1 S${2} ;single extruder fan/;
+   s/^(M106 S(\d+).*)$/$1\nM106 P1 S$2 ;single extruder fan/;
+   s/^(M107(\b.*)?)$/$1\nM107 P1 ;single extruder fan/;
    print;
 }
