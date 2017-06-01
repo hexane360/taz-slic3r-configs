281e4172bb87e2701f925c9b50638b6147c6c5a5
Colin Gilgenbach <colin@gilgenbach.net>
2017-06-01 12:09:07 -0600

    Initial fork

--- dual_to_single_fan.bat
+++ dual_to_single_fan.bat
@@ -1,24 +1,36 @@
 @echo off
-setlocal EnableDelayedExpansion
-
-FOR %%f IN (%*) DO (
-	IF EXIST %%~ff% (
-		echo Received file "%%~ff"
-		echo Modifying M106. . .
-		type "%%~ff" | call "%~dp0repl.bat" "^(M106 S(\d*).*)$" "$1\nM106 P1 S$2 ;single extruder fan" X M >"%%~ff.1" && (
-			move /y "%%~ff.1" "%%~ff"
-		) || (
-			del "%%~ff.1"
-			echo Failed to run M106 script
-		)
-		echo Modifying M107. . .
-		type "%%~ff" | call "%~dp0repl.bat" "^(M107(\D.*)?)$" "$1\nM107 P1 ;single extruder fan" X M >"%%~ff.1" && (
-			move /y "%%~ff.1" "%%~ff"
-		) || (
-			del "%%~ff.1"
-			echo Failed to run M107 script
-		)
-	) ELSE (
-		echo Unable to find file "%%~ff", skipping
+for %%f in (%*) do (
+	echo Received file "%%~ff"
+	echo Running fan script through Slic3r perl
+	"%~d0\gnmaker\Printrun\Slic3r\perl5.24.0.exe" -i.1 "%~dp0dual_to_single_fan.pl" "%%~ff" && (
+		del "%%~ff.1"
+	) || (
+		echo Failed to run fan script, error %ERRORLEVEL%
+		pause
 	)
 )
+
+:finddrive
+FOR %%d in (G F E) DO (
+	IF EXIST "%%d:"\* (
+		IF NOT "%%d:"=="%~d0" (
+			SET drive=%%d:
+			goto copyfiles
+		)
+	)
+)
+echo No external drive found.
+goto end
+
+:copyfiles
+
+for /f "tokens=2" %%a in ('wmic volume get Label^, DriveLetter ^| find "%drive%"') do set label=%%a
+echo Using drive %drive% (%label%)
+FOR %%f IN (%*) DO (
+	IF EXIST %%~ff% (
+		echo Received file "%%~nxf"
+		echo Copying to %drive%
+		copy /y "%%~ff" "%drive%"
+	)
+)
+:end
