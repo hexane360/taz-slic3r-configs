f6a547e3e0b7beb3d6dbe945e5b6db76b79dd912
Colin Gilgenbach <colin@gilgenbach.net>
2017-08-18 19:42:48 -0600

    Temporarily removed broken Windows functionality

--- dual_to_single_fan.pl
+++ dual_to_single_fan.pl
@@ -1,10 +1,10 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -i
 
 use strict;
 use warnings;
 
 while ( <> ) {
-   s/^(M106 S(\d+).*)$/$1\nM106 P1 S$2 ;single extruder fan/;
-   s/^(M107(\b.*)?)$/$1\nM107 P1 ;single extruder fan/;
+   s/^(M106 S(\d+).*)$/$1\nM106 P1 S$2 ;single extruder fan/gm;
+   s/^(M107(\b.*)?)$/$1\nM107 P1 ;single extruder fan/gm;
    print;
 }
